c555c8d4baa86c0c9c5199588244c7caabb9860c
Grant Ammons <grant@grant.dev>
2022-07-17 07:20:45 -0400

    update backup script to back up baci-smp and buckaroo town

--- minecraft/db_backup/backup.sh
+++ minecraft/db_backup/backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 _now=`date +"%Y-%m-%d-%H"`
-_file="minecraft-buckaroo-town-$_now.tgz"
+_file="minecraft-$1-$_now.tgz"
 tar -czvf $_file /data/*.json /data/server.properties /data/world
-aws s3 cp "$_file" s3://minecraft-buckaroo-town-backup/"$_file"
+aws s3 cp "$_file" s3://minecraft-$1-backup/"$_file"
