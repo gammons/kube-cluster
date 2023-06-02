8fdff676ba1c2b43d6dee82c4f9546ead0ded0eb
Grant Ammons <grant@grant.dev>
2023-06-02 06:42:21 -0400

    ensure baci smp is being backed up properly

--- minecraft/db_backup/backup.sh
+++ minecraft/db_backup/backup.sh
@@ -2,5 +2,5 @@
 
 _now=`date +"%Y-%m-%d-%H"`
 _file="minecraft-$1-$_now.tgz"
-tar -czvf $_file /data/*.json /data/server.properties /data/world
+tar -czvf $_file /data/Baci\ SMP
 aws s3 cp "$_file" s3://minecraft-$1-backup/"$_file"
